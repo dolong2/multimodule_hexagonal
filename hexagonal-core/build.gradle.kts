7b084e329e0e61a362aa13f3c9470c95fdaf3bd8
jeayoung <k01066624566@gmail.com>
2023-06-07 15:00:50 +0900

    core 모듈 gradle세팅

--- hexagonal-core/build.gradle.kts
+++ hexagonal-core/build.gradle.kts
@@ -1,7 +1,3 @@
-plugins {
-    id("java")
-}
-
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
@@ -10,8 +6,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("org.springframework:spring-tx:6.0.6")
 }
 
 tasks.getByName<Test>("test") {
