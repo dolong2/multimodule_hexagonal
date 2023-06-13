47e60d1cc2ee3c81f4b90eed45d323f906ddd27f
jeayoung <k01066624566@gmail.com>
2023-06-13 10:10:09 +0900

    add AdminDetails and MemberDetails

---
+++ hexagonal-infrastructure/src/main/kotlin/multimodule/hexagonal/global/security/auth/MemberDetails.kt
@@ -0,0 +1,25 @@
+package multimodule.hexagonal.global.security.auth
+
+import multimodule.hexagonal.domain.auth.model.Role
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class MemberDetails(
+    private val memberId: String
+) : UserDetails {
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> =
+        mutableListOf(SimpleGrantedAuthority(Role.ROLE_MEMBER.name))
+
+    override fun getPassword(): String? = null
+
+    override fun getUsername(): String = memberId
+
+    override fun isAccountNonExpired(): Boolean = true
+
+    override fun isAccountNonLocked(): Boolean = true
+
+    override fun isCredentialsNonExpired(): Boolean = true
+
+    override fun isEnabled(): Boolean = true
+}
---
+++ hexagonal-infrastructure/src/main/kotlin/multimodule/hexagonal/global/security/auth/MemberDetailsService.kt
@@ -0,0 +1,17 @@
+package multimodule.hexagonal.global.security.auth
+
+import multimodule.hexagonal.domain.member.spi.MemberPort
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Component
+
+@Component
+class MemberDetailsService(
+    private val memberPort: MemberPort
+) : UserDetailsService{
+    override fun loadUserByUsername(username: String): UserDetails {
+        if (memberPort.existsMemberId(username))
+            throw RuntimeException()// TODO username으로 조회할 수 없음
+        return MemberDetails(username)
+    }
+}
