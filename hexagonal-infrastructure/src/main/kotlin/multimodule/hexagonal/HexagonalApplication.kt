a6b256558b9534b31c799d59711d4ddacebf97d6
jeayoung <k01066624566@gmail.com>
2023-06-13 09:06:23 +0900

    add ConfigurationPropertiesScan

--- hexagonal-infrastructure/src/main/kotlin/multimodule/hexagonal/HexagonalApplication.kt
+++ hexagonal-infrastructure/src/main/kotlin/multimodule/hexagonal/HexagonalApplication.kt
@@ -1,10 +1,12 @@
 package multimodule.hexagonal
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import java.util.*
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class HexagonalApplication
 
 fun main(args: Array<String>) {
