362fe0f199d51ba89707d3f8705ac4d0176e55d0
jeayoung <k01066624566@gmail.com>
2023-06-15 07:45:11 +0900

    change javax -> jakarta

--- hexagonal-infrastructure/src/main/kotlin/multimodule/hexagonal/global/error/handler/BasicExceptionHandler.kt
+++ hexagonal-infrastructure/src/main/kotlin/multimodule/hexagonal/global/error/handler/BasicExceptionHandler.kt
@@ -1,12 +1,12 @@
 package multimodule.hexagonal.global.error.handler
 
+import jakarta.servlet.http.HttpServletRequest
 import multimodule.hexagonal.global.error.exception.BasicException
 import multimodule.hexagonal.global.error.response.ErrorResponse
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import javax.servlet.http.HttpServletRequest
 
 @RestControllerAdvice
 class BasicExceptionHandler {
