9df2213c41b6d4f45e9fae39814a58fa064163a5
jeayoung <k01066624566@gmail.com>
2023-07-04 13:16:21 +0900

    add allopen

---
+++ buildSrc/src/main/kotlin/AllOpen.kt
@@ -0,0 +1,20 @@
+object AllOpen {
+
+    // usecase
+    const val USECASE = "multimodule.hexagonal.common.annotation.UseCase"
+
+    // read only usecase
+    const val READ_ONLY_USECASE = "multimodule.hexagonal.common.annotation.ReadOnlyUseCase"
+
+    // service
+    const val SERVICE = "multimodule.hexagonal.common.annotation.Service"
+
+    // entity
+    const val ENTITY = "jakarta.persistence.Entity"
+
+    // mapped superclass
+    const val MAPPED_SUPERCLASS = "jakarta.persistence.MappedSuperclass"
+
+    // embeddable
+    const val EMBEDDABLE = "jakarta.persistence.Embeddable"
+}
--- hexagonal-core/build.gradle.kts
+++ hexagonal-core/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("org.springframework.boot") version PluginVersion.SPRING_BOOT_VERSION
     id("io.spring.dependency-management") version PluginVersion.DEPENDENCY_MANAGER_VERSION
+    kotlin("plugin.allopen") version PluginVersion.ALLOPEN_VERSION
     kotlin("plugin.spring") version PluginVersion.SPRING_PLUGIN_VERSION apply false
 }
 
@@ -11,4 +12,10 @@ repositories {
 dependencies {
     // transaction
     implementation(Dependencies.SPRING_TRANSACTION)
+}
+
+allOpen{
+    annotation(AllOpen.USECASE)
+    annotation(AllOpen.READ_ONLY_USECASE)
+    annotation(AllOpen.SERVICE)
 }
