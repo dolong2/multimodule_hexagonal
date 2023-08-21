573111a04bde033a4acbb5213af0a3f6ca0db415
jeayoung <k01066624566@gmail.com>
2023-08-21 10:45:13 +0900

    apply jpa plugin in persistence layer

--- hexagonal-persistence/build.gradle.kts
+++ hexagonal-persistence/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("org.springframework.boot") version PluginVersion.SPRING_BOOT_VERSION
     id("io.spring.dependency-management") version PluginVersion.DEPENDENCY_MANAGER_VERSION
-    kotlin("plugin.jpa") version PluginVersion.JPA_PLUGIN_VERSION apply false
+    kotlin("plugin.jpa") version PluginVersion.JPA_PLUGIN_VERSION
     kotlin("plugin.spring") version PluginVersion.SPRING_PLUGIN_VERSION apply false
 }
 
