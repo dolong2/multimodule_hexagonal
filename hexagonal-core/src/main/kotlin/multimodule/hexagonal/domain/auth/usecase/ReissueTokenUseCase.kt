4381de35a25c2d880ad4065bd0cbc193394024a1
jeayoung <k01066624566@gmail.com>
2023-08-28 09:45:40 +0900

    refactoring ReissueTokenUseCase to delete existing RefreshToken

--- hexagonal-core/src/main/kotlin/multimodule/hexagonal/domain/auth/usecase/ReissueTokenUseCase.kt
+++ hexagonal-core/src/main/kotlin/multimodule/hexagonal/domain/auth/usecase/ReissueTokenUseCase.kt
@@ -3,6 +3,7 @@ package multimodule.hexagonal.domain.auth.usecase
 import multimodule.hexagonal.common.annotation.UseCase
 import multimodule.hexagonal.domain.auth.dto.response.TokenResponseData
 import multimodule.hexagonal.domain.auth.exception.RefreshTokenExpiredException
+import multimodule.hexagonal.domain.auth.spi.CommandRefreshTokenPort
 import multimodule.hexagonal.domain.auth.spi.JwtPort
 import multimodule.hexagonal.domain.auth.spi.QueryRefreshTokenPort
 import multimodule.hexagonal.domain.member.exception.MemberNotFoundException
@@ -13,10 +14,12 @@ class ReissueTokenUseCase(
     private val queryRefreshTokenPort: QueryRefreshTokenPort,
     private val queryMemberPort: QueryMemberPort,
     private val jwtPort: JwtPort,
+    private val commandRefreshTokenPort: CommandRefreshTokenPort
 ) {
     fun execute(refreshToken: String): TokenResponseData {
         val token = (queryRefreshTokenPort.findByToken(refreshToken)
             ?: throw RefreshTokenExpiredException())
+        commandRefreshTokenPort.deleteRefreshTokenByToken(token.token)
         val userId = token.userId
         val member = (queryMemberPort.findByMemberId(userId)
             ?: throw MemberNotFoundException())
