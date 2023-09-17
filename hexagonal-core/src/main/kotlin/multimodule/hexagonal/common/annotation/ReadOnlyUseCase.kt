0bb88bb1145d93defdcfd98c77e6d43ebde10905
jeayoung <k01066624566@gmail.com>
2023-09-17 14:00:45 +0900

    add rollbackFor options to ReadOnlyUseCase annotation

--- hexagonal-core/src/main/kotlin/multimodule/hexagonal/common/annotation/ReadOnlyUseCase.kt
+++ hexagonal-core/src/main/kotlin/multimodule/hexagonal/common/annotation/ReadOnlyUseCase.kt
@@ -5,5 +5,5 @@ import org.springframework.transaction.annotation.Transactional
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.CLASS)
-@Transactional(readOnly = true)
+@Transactional(readOnly = true, rollbackFor = [Exception::class])
 annotation class ReadOnlyUseCase()
